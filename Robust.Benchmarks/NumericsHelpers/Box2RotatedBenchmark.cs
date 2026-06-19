7117148505d304507c861a197889857951634a3e
metalgearsloth <comedian_vs_clown@hotmail.com>
2026-06-19 17:08:13 +1000

    Add early-out for CalcBoundingBox

--- Robust.Benchmarks/NumericsHelpers/Box2RotatedBenchmark.cs
+++ Robust.Benchmarks/NumericsHelpers/Box2RotatedBenchmark.cs
@@ -1,4 +1,6 @@
-﻿using System.Numerics;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Runtime.Intrinsics;
 using BenchmarkDotNet.Attributes;
 using Robust.Shared.Analyzers;
 using Robust.Shared.Maths;
@@ -14,5 +16,45 @@ public class Box2RotatedBenchmark
     public Matrix3x2 GetTransform()
     {
         return Box.Transform;
+    }
+}
+
+[Virtual, DisassemblyDiagnoser]
+public class Box2RotatedBoundingBoxBenchmark
+{
+    public Box2Rotated ZeroRotationBox = new(new Box2(-1, -2, 3, 4), Angle.Zero, new Vector2(0.25f, -0.5f));
+    public Box2Rotated RotatedBox = new(new Box2(-1, -2, 3, 4), 0.7f, new Vector2(0.25f, -0.5f));
+
+    private Box2 _boxResult;
+
+    [Benchmark(Baseline = true)]
+    public void CurrentZeroRotation()
+    {
+        _boxResult = ZeroRotationBox.CalcBoundingBox();
+    }
+
+    [Benchmark]
+    public void NoCheckZeroRotation()
+    {
+        _boxResult = CalcBoundingBoxNoRotationCheck(ZeroRotationBox);
+    }
+
+    [Benchmark]
+    public void CurrentRotated()
+    {
+        _boxResult = RotatedBox.CalcBoundingBox();
+    }
+
+    [Benchmark]
+    public void NoCheckRotated()
+    {
+        _boxResult = CalcBoundingBoxNoRotationCheck(RotatedBox);
+    }
+
+    private static Box2 CalcBoundingBoxNoRotationCheck(in Box2Rotated box)
+    {
+        box.GetVertices(out var x, out var y);
+        var aabb = SimdHelpers.GetAABB(x, y);
+        return Unsafe.As<Vector128<float>, Box2>(ref aabb);
     }
 }
